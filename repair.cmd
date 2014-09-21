4003e9724c5dc785f01460eb241f3d2602a50386
Michael Shallcross <m.a.shallcross@gmail.com>
2014-09-21 15:13:02 +1000

    Repair known_hosts on Windows
    Repair known_hosts file on Windows to remove fingerprint from prior
    provisioning attempts.  (Note: Does not resolve problem with rsync from
    Windows)

--- repair.cmd
+++ repair.cmd
@@ -1,2 +1,2 @@
 REM Repair known_hosts file to remove fingerprint from previous provisioning attempts
-ssh-keygen -f "/home/Michael/.ssh/known_hosts" -R 128.199.166.172
+ssh-keygen -f "/home/shallcrm/.ssh/known_hosts" -R 128.199.166.172
